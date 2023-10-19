be811b31ef4c6f6ff06dd4c52c8384baf3704e55
Murilo <murilo.ti32@outlook.com>
2023-10-19 10:40:51 -0300

    function of added task in list

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const button = document.querySelector('.button-add-task')
 const input  = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
 
 let minhaListaDeItens = []
-
-
 
 function adicionarNovaTarefa() {
     minhaListaDeItens.push(input.value)
@@ -13,13 +12,20 @@ function adicionarNovaTarefa() {
 
 function mostrarTarefas() {
 
-    let novaLi
+    let novaLi = ''
 
-    // <li class="task">
-    //             <img src="assets/checked.png" alt="Checked-na-tarefa">
-    //             <p>Aprender JS</p>
-    //             <img src="assets/trash.png" alt="Lixeira">                
-    //         </li>    
+    minhaListaDeItens.forEach( tarefa => {
+
+        novaLi = novaLi + `
+            <li class="task">
+                <img src="assets/checked.png" alt="Checked-na-tarefa">
+                <p>${tarefa}</p>
+                <img src="assets/trash.png" alt="Lixeira">                
+            </li>            
+        `
+    })
+
+    listaCompleta.innerHTML = novaLi
 
 }
 
